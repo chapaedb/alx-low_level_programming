75a023ecf4c90903dc890accf23126f87013bdd8
chapaedb <chapaeresso1@gmail.com>
2023-08-15 12:52:23 -0700

    1st commit on struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 *
+	 * Description:Define a new type struct dog  
+	 */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+#endif
