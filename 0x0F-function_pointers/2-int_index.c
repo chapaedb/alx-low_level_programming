c19dcbafe3123693095ed04ab63c9d7642e327d0
chapaedb <chapaeresso1@gmail.com>
2023-08-16 14:45:44 -0700

    task 2 dupdate done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
- * int_index -  check if a number is equal to 98
- * @my_array:list
- * @size:lenght of the array
- * @cmp: pointer to func
- * Return: 0 if false, something else otherwise.
+ * int_index - return index place if comparison = true, else -1
+ * @my_array: list
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
  */
 int int_index(int *my_array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (my_array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(my_array[i]))
 			return (i);
 	}
 	return (-1);
 }
+
