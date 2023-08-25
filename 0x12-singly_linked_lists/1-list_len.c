81e09501a2d27e1efa7ffa06e02d370a76facb0b
chapaedb <chapaeresso1@gmail.com>
2023-08-25 07:08:59 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,12 @@
-#include"lists.h"
+#include <stddef.h>
+#include "lists.h"
 /**
  * list_length - Returns the number of nodes in a linked list.
  * @head: The head of the linked list.
  *
  * Return: The number of nodes in the linked list.
  */
-size_t list_length(const list_t *head)
+size_t list_len(const list_t *head)
 {
 	size_t n = 0;
 	const list_t *current = head;
--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 #include "lists.h"
+
 
 /**
  * main - check the code
