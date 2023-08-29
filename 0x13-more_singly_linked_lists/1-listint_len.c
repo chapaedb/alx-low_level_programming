8e539179e2c72e2d7dac2b89d0e1df3accc061c1
chapaedb <chapaeresso1@gmail.com>
2023-08-29 14:52:50 -0700

    Betty added

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,9 +11,10 @@ size_t listint_len(const listint_t *h)
 	size_t count = 0;
 
 	while (h != NULL)
-{
-	count++;
-	h = h->next;
-}
-return (count);
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
