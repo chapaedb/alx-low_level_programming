d40e6e78ff8b87fc2e1c633d099253872db156fa
chapaedb <chapaeresso1@gmail.com>
2023-08-29 11:58:00 -0700

    Added betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,16 +9,15 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *temp;
+	listint_t *temp;
 
     temp = malloc(sizeof(listint_t));
-
     if (temp == NULL)
-        return NULL;
-
+	    return NULL;
     temp->n = n;
+
     temp->next = *head;
     *head = temp;
 
-    return temp;
+    return (temp);
 }
